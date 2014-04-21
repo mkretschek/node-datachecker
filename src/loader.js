b719f415070ae8129b442a1baedc1666f2269150
Mathias Kretschek <mathias@kretschek.com.br>
2014-04-21 20:57:22 -0300

    Initial loader documentation

--- src/loader.js
+++ src/loader.js
@@ -1,8 +1,9 @@
 /**
- * This module defines methods for loading formats from format files.
+ * Methods for loading formats from format files.
  */
 
 'use strict';
+
 
 var
   _ = require('underscore'),
@@ -17,11 +18,21 @@ var
 require('js-yaml');
 
 
+/**
+ * Loads the given format file.
+ * @param {String} file Path to the format file.
+ * @return {Object} The parsed format object.
+ */
 function loadFormatFile(file) {
   return require(file);
 }
 
 
+/**
+ * Loads all format files in the given dir.
+ * @param {String} dir Path to the dir containing the format files.
+ * @return {Object} The parsed format object.
+ */
 function loadFormatDir(dir) {
   var
     formats = {},
@@ -47,34 +58,16 @@ function loadFormatDir(dir) {
 }
 
 
-function loadFormats(path) {
-  // This is supposed to be called as a step of the initialization process,
-  // and the application should not work while the format files have not
-  // been loaded. Therefore, we use the Sync version of `fs` methods.
-
-  var
-    formats,
-    stats;
-
-  if (Array.isArray(path)) {
-    formats = {};
-
-    _.each(path, function (val) {
-      utils.deepExtend(formats, loadFormats(val));
-    });
-  } else {
-    stats = fs.statSync(path);
-
-    formats = stats.isDirectory() ?
-      loadFormatDir(path) :
-      loadFormatFile(path);
-  }
-
-  return simplifyFormats(formats);
-}
 
 
-
+/**
+ * Simplifies the format objects joining custom types until all formats
+ * use native javascript types, avoiding the overhead of doing the join on each
+ * check.
+ * @param {Object} formats A complete format object as returned from
+ *  {@code loadFormats()}.
+ * @return {Object} A simplified format object.
+ */
 function simplifyFormats(formats) {
   var
     simplified = {},
@@ -82,6 +75,13 @@ function simplifyFormats(formats) {
     format;
 
 
+  /**
+   * Simplifies a specific format definition, resolving custom types into native
+   * javascript types, merging specifications into a single object and expanding
+   * property formats.
+   * @param {Object} format The specific format to simplify.
+   * @return {Object} The simplified format.
+   */
   function simplify(format) {
     var
       simplified,
@@ -115,6 +115,39 @@ function simplifyFormats(formats) {
   return simplified;
 }
 
+
+
+/**
+ * Loads all formats from the given path (or array of paths).
+ * @param {(String|Array.<String>)} path Path or array of paths to files containing
+ *  format definitions.
+ * @return {Object} A simplified format object.
+ */
+function loadFormats(path) {
+  // This is supposed to be called as a step of the initialization process,
+  // and the application should not work while the format files have not
+  // been loaded. Therefore, we use the Sync version of `fs` methods.
+
+  var
+    formats,
+    stats;
+
+  if (Array.isArray(path)) {
+    formats = {};
+
+    _.each(path, function (val) {
+      utils.deepExtend(formats, loadFormats(val));
+    });
+  } else {
+    stats = fs.statSync(path);
+
+    formats = stats.isDirectory() ?
+      loadFormatDir(path) :
+      loadFormatFile(path);
+  }
+
+  return simplifyFormats(formats);
+}
 
 
 module.exports = loadFormats;
